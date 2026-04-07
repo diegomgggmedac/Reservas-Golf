9f3ab1e934daf821b3bd488285ece4bc789b72d9
CristinaPG26 <cpg0027@alu.medac.es>
2026-04-07 10:36:59 +0200

    cambios de carpeta build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,47 +1,44 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Asegúrate de que esta versión sea compatible con tu versión de AGP
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.gms:google-services:4.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+//val customBuildDirProvider = rootProject.layout.buildDirectory.dir("../../build")
+val customBuildPath = layout.projectDirectory.dir("../../build")
+
+rootProject.layout.buildDirectory.set(customBuildPath)
+
+subprojects {
+    val newSubprojectBuildDir = customBuildPath.map { it.dir(project.name) }
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    if (project.path != ":app") {
+        evaluationDependsOn(":app")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.reservas_golf"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.reservas_golf"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
+    id("com.google.gms.google-services") // <--- Sin esto, el JSON es invisible
 }
